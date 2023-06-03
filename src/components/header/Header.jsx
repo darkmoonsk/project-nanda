2bb77d00823d19fd05c101349d542f60fe564beb
Bruno Souza <brunoezully@gmail.com>
2023-06-03 17:29:35 -0300

    Fix: Ajustes na responsividade do Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,14 +4,14 @@ function Header() {
     return (
         <header
             className="
-                flex flex-col gap-12 w-full h-64 items-end justify-between    
+                flex flex-col gap-12 w-full min-h-64 items-center justify-between    
                 pl-4 pr-4 pb-6 border-pele-escuro border-b-2           
-                lg:flex-row lg:pl-32 lg:pr-32
+                lg:flex-row lg:pl-32 lg:pr-32 lg:items-end
                 "
         >
             <img
                 className="w-96 rounded-3xl opacity-60 drop-shadow-xl"
-                src="./src/assets/logo.png"
+                src="/src/assets/logo.png"
                 alt="logo"
             />
             <Menu />
