354d854996c652738cc55c16b76cafa9b26a446f
Bruno Souza <brunoezully@gmail.com>
2023-07-17 16:54:46 -0300

    Feat: primeiro modulo de idioma: português

---
+++ src/languages/portuguese.jsx
@@ -0,0 +1,9 @@
+export default {
+    about: {
+        title: "Sobre mim",
+        description: [
+            "Olá, meu nome é Fernanda Schubert, nascida e criada no Brasil, agora baseada na Alemanha. Com anos de experiência em ensino e um amor profundo por compartilhar conhecimento, eu me dedico a oferecer aulas de alta qualidade e personalizadas para meus alunos.",
+            "Minha jornada me levou por diversas culturas e países, enriquecendo minha própria compreensão do mundo e, consequentemente, a maneira como ensino. Esse percurso multicultural moldou minha abordagem de ensino, permitindo-me entender e se adaptar a diferentes estilos de aprendizagem."
+        ],
+    },
+};
