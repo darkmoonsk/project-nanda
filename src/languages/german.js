f866c62857cca7271dd73f3e75cc982e0f4c12f8
Bruno Souza <brunoezully@gmail.com>
2023-07-21 02:00:49 -0300

    Feat: Adicionado Idioma Alemão

---
+++ src/languages/german.js
@@ -0,0 +1,47 @@
+export default {
+    lang: "de",
+    langs: {
+        portuguese: "Portugiesisch",
+        german: "Deutsch",
+        spanish: "Spanisch"
+    },
+    navbar: {
+        home: "Start",
+        about: "Über mich",
+        contact: "Kontakt"
+    },
+    banner: {
+        description: "Öffnen Sie Wege, überschreiten Sie Grenzen und erkunden Sie die Welt durch die Fließfähigkeit in einer neuen Sprache mit Fernanda Schubert.",
+        slogan: "Worte in Brücken zu neuen Möglichkeiten verwandeln"
+    },    
+    about: {
+        title: "Über mich",
+        description: [
+            "Hallo, mein Name ist Fernanda Schubert, geboren und aufgewachsen in Brasilien, jetzt in Deutschland ansässig. Mit jahrelanger Erfahrung im Unterrichten und einer tiefen Liebe zum Wissenstransfer, widme ich mich dem Anbieten von hochqualitativen, maßgeschneiderten Unterrichtsstunden für meine Schüler.",
+            "Meine Reise hat mich durch verschiedene Kulturen und Länder geführt und damit mein Verständnis von der Welt und somit meine Unterrichtsmethoden bereichert. Dieser multikulturelle Weg hat meine Unterrichtsansätze geprägt und es mir ermöglicht, mich an unterschiedliche Lernstile anzupassen.",
+        ],
+    },
+    services: {
+        title: "Dienstleistungen",
+        description: [
+            "Ich biete Privatunterricht für Einzelpersonen und Gruppen an, zugeschnitten auf die speziellen Bedürfnisse jedes Schülers. Ob für persönliche oder berufliche Weiterbildung, mein Unterricht konzentriert sich darauf, eine ansprechende und effektive Lernerfahrung zu bieten.",
+            "Für Unternehmen, die ihren Mitarbeitern die Vorteile des Lernens anbieten möchten, biete ich maßgeschneiderte Pakete an, die an die Bedürfnisse Ihrer Organisation angepasst werden können.",
+        ],
+    },
+    slide: {
+        portuguese: {
+            title: "Portugiesisch lernen",
+            slogan: "Entdecken Sie eine neue Welt voller lebhafter Kulturen und unglaublicher Möglichkeiten - Lernen Sie heute!"
+        },
+        german: {
+            title: "Deutsch lernen",
+            slogan: "Entdecken Sie die Geheimnisse des Herzens Europas - lernen Sie Deutsch und öffnen Sie Türen zu neuen Abenteuern!"
+        },
+        spanish: {
+            title: "Spanisch lernen",
+            slogan: "Vernetzen Sie sich mit Millionen von Menschen auf der ganzen Welt - sprechen Sie die Sprache der Liebe, lernen Sie heute Spanisch!"
+        },
+    
+    }
+}
+
