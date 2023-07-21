5cf0a939c0efa63dd0b6f05dedda59b208c315f4
Bruno Souza <brunoezully@gmail.com>
2023-07-21 02:01:11 -0300

    Feat: Adicionado idioma Espanhol

---
+++ src/languages/spanish.js
@@ -0,0 +1,47 @@
+export default {
+    lang: "es",
+    langs: {
+        portuguese: "Portugués",
+        german: "Alemán",
+        spanish: "Español"
+    },
+    navbar: {
+        home: "Inicio",
+        about: "Sobre mí",
+        contact: "Contacto"
+    },
+    banner: {
+        description: "Abre caminos, cruza fronteras y explora el mundo a través de la fluidez en un nuevo idioma con Fernanda Schubert.",
+        slogan: "Transformando palabras en puentes para nuevas oportunidades"
+    },      
+    about: {
+        title: "Sobre mí",
+        description: [
+            "Hola, mi nombre es Fernanda Schubert, nacida y criada en Brasil, ahora con base en Alemania. Con años de experiencia en enseñanza y un profundo amor por compartir conocimientos, me dedico a ofrecer clases de alta calidad y personalizadas para mis estudiantes.",
+            "Mi viaje me ha llevado a través de varias culturas y países, enriqueciendo mi propia comprensión del mundo y, en consecuencia, la forma en que enseño. Este recorrido multicultural ha moldeado mi enfoque de enseñanza, permitiéndome entender y adaptarme a diferentes estilos de aprendizaje.",
+        ],
+    },
+    services: {
+        title: "Servicios",
+        description: [
+            "Ofrezco clases particulares para individuos y grupos, adaptadas a las necesidades específicas de cada estudiante. Ya sea para mejora personal o profesional, mis clases se centran en proporcionar una experiencia de aprendizaje atractiva y efectiva.",
+            "Para las empresas que desean ofrecer el beneficio del aprendizaje a sus empleados, ofrezco paquetes personalizados que pueden ser adaptados de acuerdo con las necesidades de su organización.",
+        ],
+    },
+    slide: {
+        portuguese: {
+            title: "Aprende Portugués",
+            slogan: "Desbloquea un nuevo mundo de culturas vibrantes y oportunidades increíbles - ¡Aprende hoy!"
+        },
+        german: {
+            title: "Aprende Alemán",
+            slogan: "Desvela los secretos del corazón de Europa - aprende alemán y abre puertas a nuevas aventuras!"
+        },
+        spanish: {
+            title: "Aprende Español",
+            slogan: "Conéctate con millones de personas en todo el mundo - habla el idioma del amor, ¡aprende español hoy!"
+        },
+    
+    }
+}
+
